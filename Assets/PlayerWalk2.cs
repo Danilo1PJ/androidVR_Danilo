ccf021ca063534f2f21a7bae68dc636663cb1432
Danilo1PJ <danilormorais@hotmail.com>
2020-12-07 22:35:39 -0300

    Controle Camera no GoogleVR

---
+++ Assets/PlayerWalk2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk2 : MonoBehaviour
+{
+    public int playerSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+    }
+}
