b7cf47d2955c0a1efb16cd2e928ad385c6873eed
Danilo1PJ <danilormorais@hotmail.com>
2020-12-21 12:32:45 -0300

    APK Castelo

---
+++ Assets/EndOfLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevel : MonoBehaviour
+{
+    public void EndOfGame()
+    {
+        //Debug.Log("HIT");
+        //Application.Quit();
+        SceneManager.LoadScene("fps");
+    }
+}
---
+++ Assets/RotateCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float SpinForce;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, SpinForce * Time.deltaTime, 0);
+    }
+
+    public void ChangeSpin()
+    {
+        SpinForce = -SpinForce;
+    }
+}
